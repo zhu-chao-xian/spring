a18770c38f634d91ead259d1f3769c262a79dc27
zhu_c <zhu_c@centit.com>
2020-03-31 18:11:38 +0800

    132423

--- demo/src/main/java/com/study/demo/sevice/UserService.java
+++ demo/src/main/java/com/study/demo/sevice/UserService.java
@@ -5,6 +5,11 @@ import com.study.demo.pojo.UserInfo;
 
 public interface UserService {
 
+    /**
+     * 通过id获取用户信息
+     * @param id
+     * @return
+     */
     UserInfo getUserById(Integer id);
 
     boolean set(String key);
